b1f356dc3341f88efff5296bdacfd241f8679468
Derek Geslison <geslisond@gmail.com>
2025-08-26 19:09:30 -0600

    Move index.css to src/, add Tailwind config, update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// src/index.js (boilerplate)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Include Tailwind here
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
